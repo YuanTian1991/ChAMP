aa2b368b1b622d1c531bea99a081984a32d8f4c9
Tiffany Morris <Tiffany.Morris@ucl.ac.uk>
2014-06-11 15:33:01 +0000

    Fixed bug in champ.read
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChAMP@91272 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/champ.read.R
+++ R/champ.read.R
@@ -1,8 +1,8 @@
 champ.read <-
 function(betaFile="beta.txt",sampleSheet="sampleSheet.txt",resultsDir)
 {
-	beta <-read.table(betaFile,row.names =T, sep = "\t")
-	pd <- read.table(sampleSheet) #need to check
+	beta <-read.table(betaFile,header =T, sep = "\t")
+	pd <- read.table(sampleSheet,header=T,sep="\t")
 	if(file.exists(resultsDir))
 	{
 		message("The directory ",resultsDir," already exists. To avoid overwriting please rename in the arguments or delete this folder.")
