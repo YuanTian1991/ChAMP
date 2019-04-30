11cba249e3b4a837be5319a3a59685dacdfd5b35
Rasmus Rydbirk <rasmus.rydbirk@regionh.dk>
2019-04-30 16:04:59 +0200

    Update champ.SVD.R

--- R/champ.SVD.R
+++ R/champ.SVD.R
@@ -143,10 +143,43 @@ champ.SVD <- function(beta=myNorm,
         legend(x=-(topPCA/2.5),y=3,legend=c(expression("p < 1x"~10^{-10}),expression("p < 1x"~10^{-5}),"p < 0.01", "p < 0.05", "p > 0.05"), fill=c("darkred","red","orange","pink","white"),par('usr')[2], par('usr')[4], xpd=NA);    
     }
       #Screeplot start
-	splot <- function(x=svd.o,y=rmt.o)  
+	       ##svd.scree function from outdated svdvis package##
+	svd.scree <- function(svd.obj, subr=NULL, maintitle="Scree Plot", axis.title.x="Singular Vectors", axis.title.y="Percent Variance Explained") {
+  if(is.list(svd.obj) & all(names(svd.obj) %in% c("u","d","v"))) {
+    print("Your input data is treated as a SVD output, with u, d, v corresponding to left singular vector, singular values, and right singular vectors, respectively.")
+  } else {
+    print("Your input data is treated as a vector of singular values. For example, it should be svd.obj$d from a SVD output.")
+    svd.obj = list(d=svd.obj)
+  }
+
+  print("Scree Plot")
+
+  if(is.null(names(svd.obj$d))) {
+    names(svd.obj$d) = paste0("V",1:ncol(svd.obj$v))
+  }
+
+  pve = svd.obj$d^2/sum(svd.obj$d^2) * 100
+  pve = data.frame(names=names(svd.obj$d), pve)
+  pve$names = factor(pve$names, levels=unique(names(svd.obj$d)))
+  g = ggplot(pve, aes(names, pve)) + geom_point() + theme_bw()
+  gout = g + ylim(0,NA) +
+    labs(title=maintitle, axis.title.x=axis.title.x, axis.title.y=axis.title.y)
+
+  if(!is.null(subr)) {
+    gsub = g + coord_cartesian(xlim = c(0.5, subr+.5)) + ylim(min(pve$pve[1:subr])-1, max(pve$pve[1:subr])+1) +
+      labs(title=paste("First",subr,"singular values"), axis.title.x=axis.title.x, axis.title.y=axis.title.y)
+    gout = grid.arrange(gout, gsub, nrow=1)
+  }
+
+  return(gout)
+		
+}
+	       ##svd.scree end##
+	       
+	       splot <- function(x=svd.o,y=rmt.o)  
 	{
     	scp <- list(u=x$u[1:nrow(x$u),1:y$dim],v=x$v[1:y$dim,1:y$dim],d=x$d[1:y$dim])
-    	return(invisible(capture.output(svdvis::svd.scree(scp))))
+    	return(invisible(capture.output(svd.scree(scp))))
   	}
 if(Splot)
   {
